bb0c42ae025d2ed8d52cc54d24e4c575fd459941
Sparfell <arsparfell19@gmail.com>
2015-08-28 18:42:46 +0200

    ASRAI patch PBO initial commit
    Server side PBO for CUP-ASRAI compatibility

---
+++ CPC_Factions_CUP_ASRAI_patch/USMC_units.hpp
@@ -0,0 +1,61 @@
+	class SoldierWB;
+	class CUP_Creatures_Military_USMC_Soldier_Base: SoldierWB
+	{
+		asr_ai_level = 4;
+	};
+	class CUP_B_USMC_Soldier_01;
+	class CUP_B_USMC_Officer: CUP_B_USMC_Soldier_01
+	{
+		asr_ai_level = 3;
+	};
+	class CUP_B_USMC_Soldier_02;
+	class CUP_B_USMC_Soldier_SL: CUP_B_USMC_Soldier_02
+	{
+		asr_ai_level = 3;
+	};
+	class CUP_B_USMC_Soldier_07;
+	class CUP_B_USMC_Soldier_TL: CUP_B_USMC_Soldier_07
+	{
+		asr_ai_level = 3;
+	};
+	class CUP_B_USMC_Soldier_08;
+	class CUP_B_USMC_Spotter: CUP_B_USMC_Soldier_08
+	{
+		asr_ai_level = 2;
+	};
+	class CUP_B_USMC_Sniper_M40A3: CUP_B_USMC_Soldier_08
+	{
+		asr_ai_level = 10;
+	};
+	class CUP_B_USMC_Sniper_M107: CUP_B_USMC_Sniper_M40A3
+	{
+		asr_ai_level = 10;
+	};
+	class CUP_B_USMC_Soldier_03;
+	class CUP_B_USMC_Soldier_Marksman: CUP_B_USMC_Soldier_03
+	{
+		asr_ai_level = 3;
+	};
+	class CUP_B_USMC_SpecOps: CUP_B_USMC_Soldier_07
+	{
+		asr_ai_level = 3;
+	};
+	class CUP_B_USMC_Soldier_06;
+	class CUP_B_USMC_Pilot: CUP_B_USMC_Soldier_06
+	{
+		asr_ai_level = 8;
+	};
+	class CUP_Creatures_Military_FR_Soldier_Base: SoldierWB
+	{
+		asr_ai_level = 2;
+	};
+	class CUP_B_FR_Soldier_01;
+	class CUP_B_FR_Soldier_TL: CUP_B_FR_Soldier_01
+	{
+		asr_ai_level = 1;
+	};
+	class CUP_B_FR_Soldier_03;
+	class CUP_B_FR_Commander: CUP_B_FR_Soldier_03
+	{
+		asr_ai_level = 1;
+	};
---
+++ CPC_Factions_CUP_ASRAI_patch/config.cpp
@@ -0,0 +1,15 @@
+
+class CfgPatches {
+	class CPC_Factions_CUP_ASRAI_patch {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"CUP_Creatures_Military_USMC"};
+	};
+};
+
+
+class CfgVehicles
+{
+#include "USMC_units.hpp"	
+};
