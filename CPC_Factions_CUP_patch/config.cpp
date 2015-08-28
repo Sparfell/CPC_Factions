c8b2713737a5fca7688c54568a79afcb098af358
Sparfell <arsparfell19@gmail.com>
2015-08-28 11:29:47 +0200

    Initial Commit by Sparfell
    mostly CUP patches

---
+++ CPC_Factions_CUP_patch/config.cpp
@@ -0,0 +1,112 @@
+
+class CfgPatches {
+	class CPC_Factions_CUP_patch {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"cba_xeh", "CUP_Creatures_People_Military_DummyInfantrySet", "A3_Characters_F", "TRYK_Uniform", "maa_Uniform", "TRYK_BackPack", "TRYK_Shemaghs", "Kio_Balaclava", "US_ESS_Glasses_tryk", "TRYK_Uniform"};
+	};
+};
+
+class cfgWeapons
+{
+#include "cup_talibs_weap.hpp"
+};
+
+class CfgVehicles
+{
+#include "hide_cup_dummy.hpp"
+#include "cup_talibs_veh.hpp"	
+};
+
+class CfgGlasses
+{
+#include "cup_talibs_glasses.hpp"
+};
+
+class Extended_Init_Eventhandlers
+{
+	class CUP_I_TK_GUE_Soldier_Base
+	{
+		CPC_Factions_CUP_Patch_init = "if (local (_this select 0)) then {[(_this select 0), [], []] call BIS_fnc_unitHeadgear;};";
+	}
+};
+
+class CfgFaces
+{
+	class Default;
+	class Man_A3: Default
+	{
+		class PersianHead_A3_01;
+		class CPC_PersianHead_A3_01 :PersianHead_A3_01
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class CPC_PersianHead_A3_01a :PersianHead_A3_01
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class CPC_PersianHead_A3_01b :PersianHead_A3_01
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class CPC_PersianHead_A3_01c :PersianHead_A3_01
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class GreekHead_A3_02;
+		class CPC_GreekHead_A3_02 :GreekHead_A3_02
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class GreekHead_A3_03;
+		class CPC_GreekHead_A3_03 :GreekHead_A3_03
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class GreekHead_A3_06;
+		class CPC_GreekHead_A3_06 :GreekHead_A3_06
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class WhiteHead_16;
+		class CPC_WhiteHead_16 :WhiteHead_16
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class PersianHead_A3_02;
+		class CPC_PersianHead_A3_02 :PersianHead_A3_02
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class CPC_PersianHead_A3_02a :PersianHead_A3_02
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class CPC_PersianHead_A3_02b :PersianHead_A3_02
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class CPC_PersianHead_A3_02c :PersianHead_A3_02
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class PersianHead_A3_03;
+		class CPC_PersianHead_A3_03 :PersianHead_A3_03
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class CPC_PersianHead_A3_03a :PersianHead_A3_03
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class CPC_PersianHead_A3_03b :PersianHead_A3_03
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+		class CPC_PersianHead_A3_03c :PersianHead_A3_03
+		{
+			identityTypes[] = {"CPC_TAKI_head"};
+		};
+	};
+};
---
+++ CPC_Factions_CUP_patch/cup_talibs_weap.hpp
@@ -0,0 +1,90 @@
+	/*extern*/ class Uniform_Base;
+	/*extern*/ class UniformItem;
+	class CPC_TALIB_TRYK_ZARATAKI: Uniform_Base
+	{
+		scope = 1;
+		scopeCurator = 1;
+		scopeArsenal = 1;
+		displayName = "CPC TRYK Takistan Soldier 1";
+		picture = "\tryk_unit\data\ui\icon_loc.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_blufor_diver";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "I_CPC_TALIB_base0_F";
+			containerClass = "Supply80";
+			mass = 40;
+		};
+	};
+	
+	class CPC_TALIB_TRYK_ZARATAKI2: Uniform_Base
+	{
+		scope = 1;
+		scopeCurator = 1;
+		scopeArsenal = 1;
+		displayName = "CPC TRYK Takistan Soldier 2";
+		picture = "\tryk_unit\data\ui\icon_loc.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_blufor_diver";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "I_CPC_TALIB_base1_F";
+			containerClass = "Supply80";
+			mass = 40;
+		};
+	};
+	
+	class CPC_TALIB_TRYK_ZARATAKI3: Uniform_Base
+	{
+		scope = 1;
+		scopeCurator = 1;
+		scopeArsenal = 1;
+		displayName = "CPC TRYK Takistan Soldier 3";
+		picture = "\tryk_unit\data\ui\icon_loc.paa";
+		model = "\A3\Characters_F\Common\Suitpacks\suitpack_blufor_diver";
+		class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "I_CPC_TALIB_base2_F";
+			containerClass = "Supply80";
+			mass = 40;
+		};
+	};
+	
+	
+	
+	class TRYK_H_pakol2;
+	class CPC_TRYK_H_pakol2 :TRYK_H_pakol2 {
+		scope = 1;
+		scopeCurator = 1;
+		scopeArsenal = 1;
+		allowedFacewear[] = {"CPC_TRYK_Shemagh","CPC_TRYK_Shemagh_G","CPC_TRYK_Shemagh_mesh","CPC_CUP_FR_NeckScarf","CPC_CUP_FR_NeckScarf2","CPC_TRYK_Beard_BK","CPC_TRYK_Beard_BK2","CPC_TRYK_Beard_BK3","CPC_TRYK_Beard_BK4"};		
+	};
+	class TRYK_H_wig;
+	class CPC_TRYK_H_wig :TRYK_H_wig {
+		scope = 1;
+		scopeCurator = 1;
+		scopeArsenal = 1;
+		allowedFacewear[] = {"CPC_TRYK_Shemagh","CPC_TRYK_Shemagh_G","CPC_TRYK_Shemagh_mesh","CPC_CUP_FR_NeckScarf","CPC_CUP_FR_NeckScarf2","CPC_TRYK_Beard_BK","CPC_TRYK_Beard_BK2","CPC_TRYK_Beard_BK3","CPC_TRYK_Beard_BK4"};		
+	};
+	class H_ShemagOpen_tan;
+	class CPC_H_ShemagOpen_tan :H_ShemagOpen_tan {
+		scope = 1;
+		scopeCurator = 1;
+		scopeArsenal = 1;
+		allowedFacewear[] = {};		
+	};
+	class H_Shemag_olive;
+	class CPC_H_Shemag_olive :H_Shemag_olive {
+		scope = 1;
+		scopeCurator = 1;
+		scopeArsenal = 1;
+		allowedFacewear[] = {};		
+	};
+	class H_ShemagOpen_khk;
+	class CPC_H_ShemagOpen_khk :H_ShemagOpen_khk {
+		scope = 1;
+		scopeCurator = 1;
+		scopeArsenal = 1;
+		allowedFacewear[] = {};		
+	};
